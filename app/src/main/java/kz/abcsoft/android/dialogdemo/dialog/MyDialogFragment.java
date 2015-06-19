3e616cd93edfc7107404b5e465191ff9f6247e84
Daulet Baimbetov <daucloudlab@gmail.com>
2015-06-19 12:04:03 +0600

    (version 03 setItem) Dialog simple demo

--- app/src/main/java/kz/abcsoft/android/dialogdemo/dialog/MyDialogFragment.java
+++ app/src/main/java/kz/abcsoft/android/dialogdemo/dialog/MyDialogFragment.java
@@ -12,34 +12,22 @@ import android.widget.Toast;
 import kz.abcsoft.android.dialogdemo.R;
 
 public class MyDialogFragment extends DialogFragment {
-    String title = "Выбор есть всегда";
-    String message = "Выбери пищу";
-    String button1String = "Вкусная пища";
-    String button2String = "Здоровая пища";
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity()) ;
-        builder.setTitle(title) ;
-        builder.setMessage(message) ;
-        builder.setIcon(R.mipmap.ic_launcher) ;
-        builder.setPositiveButton(button1String, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(getActivity(), "Вы сделали правильный выбор",
-                        Toast.LENGTH_LONG).show();
-            }
-        }) ;
-        builder.setNegativeButton(button2String, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(getActivity(), "Возможно вы правы", Toast.LENGTH_LONG)
-                        .show();
-            }
-        }) ;
+        final String[] catNamesArray = {"Васька", "Рыжик", "Мурзик"};
 
-        builder.setCancelable(true) ;
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Выберите Кота") ;
+        builder.setIcon(R.mipmap.ic_launcher);
+        builder.setItems(catNamesArray, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getActivity(), "Выбранный кот: " + catNamesArray[i],
+                        Toast.LENGTH_SHORT).show();
+            }
+        }) ;
         return builder.create() ;
     }
 }

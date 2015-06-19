d23320a9b501c4eef161779d3c5dc5556a0532b2
Daulet Baimbetov <daucloudlab@gmail.com>
2015-06-19 11:49:10 +0600

    (version 02) Dialog simple demo

--- app/src/main/java/kz/abcsoft/android/dialogdemo/dialog/MyDialogFragment.java
+++ app/src/main/java/kz/abcsoft/android/dialogdemo/dialog/MyDialogFragment.java
@@ -7,24 +7,39 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.widget.Toast;
 
 import kz.abcsoft.android.dialogdemo.R;
 
 public class MyDialogFragment extends DialogFragment {
+    String title = "Выбор есть всегда";
+    String message = "Выбери пищу";
+    String button1String = "Вкусная пища";
+    String button2String = "Здоровая пища";
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity()) ;
-        builder.setTitle("Маңызды хабарлама") ;
-        builder.setMessage("Покормить кота!") ;
+        builder.setTitle(title) ;
+        builder.setMessage(message) ;
         builder.setIcon(R.mipmap.ic_launcher) ;
-        builder.setPositiveButton("OK, Асханаға кеттім", new DialogInterface.OnClickListener() {
+        builder.setPositiveButton(button1String, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-
+                Toast.makeText(getActivity(), "Вы сделали правильный выбор",
+                        Toast.LENGTH_LONG).show();
+            }
+        }) ;
+        builder.setNegativeButton(button2String, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getActivity(), "Возможно вы правы", Toast.LENGTH_LONG)
+                        .show();
             }
         }) ;
 
+        builder.setCancelable(true) ;
         return builder.create() ;
     }
 }
